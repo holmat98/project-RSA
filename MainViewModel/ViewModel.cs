484f66cf36783a73a2d525332053cd860291144c
holmat98 <62332336+holmat98@users.noreply.github.com>
2020-05-20 11:54:54 +0200

    fixed decoding

--- MainViewModel/ViewModel.cs
+++ MainViewModel/ViewModel.cs
@@ -57,8 +57,41 @@ namespace projekt.MainViewModel
             }
         }
 
-        private int? _e;
-        public int? E
+        private int _d1;
+        public int D1
+        {
+            get => _d1;
+            set
+            {
+                _d1 = value;
+                onPropertyChanged(nameof(D1));
+            }
+        }
+
+        private int _n;
+        public int N
+        {
+            get => _n;
+            set
+            {
+                _n = value;
+                onPropertyChanged(nameof(N));
+            }
+        }
+
+        private int _n1;
+        public int N1
+        {
+            get => _n1;
+            set
+            {
+                _n1 = value;
+                onPropertyChanged(nameof(N1));
+            }
+        }
+
+        private int _e;
+        public int E
         {
             get => _e;
             set
@@ -68,14 +101,14 @@ namespace projekt.MainViewModel
             }
         }
 
-        private int? _n;
-        public int? N
+        private string _filePath3;
+        public string FilePath3
         {
-            get => _n;
+            get => _filePath3;
             set
             {
-                _n = value;
-                onPropertyChanged(nameof(N));
+                _filePath3 = value;
+                onPropertyChanged(nameof(FilePath3));
             }
         }
 
@@ -88,13 +121,33 @@ namespace projekt.MainViewModel
                 if(_kodowanie == null)
                 {
                     _kodowanie = new RelayCommand(
-                        arg => {
+                        arg =>
+                        {
                             KodowanieRSA(FilePath, P, Q);
                         },
-                        arg => true
+                        arg => FilePath != null && P != null
                         );
                 }
                 return _kodowanie;
+            }
+        }
+
+        private ICommand _kodowanie1 = null;
+        public ICommand Kodowanie1
+        {
+            get
+            {
+                if (_kodowanie1 == null)
+                {
+                    _kodowanie1 = new RelayCommand(
+                        arg => {
+                            KodowanieRSA1(FilePath3, E, N1);
+                            MessageBox.Show("Zaszyfrowano");
+                        },
+                        arg => FilePath3 != null && E != 0
+                        );
+                }
+                return _kodowanie1;
             }
         }
 
@@ -107,9 +160,10 @@ namespace projekt.MainViewModel
                 {
                     _dekodowanie = new RelayCommand(
                         arg => {
-                            DekodowanieRSA(FilePath2, E, N);
+                            DekodowanieRSA(FilePath2, D1, N);
+                            MessageBox.Show("Rozszyfrowano");
                         },
-                        arg => true
+                        arg => FilePath2 != null && D1 != 0
                         );
                 }
 
@@ -120,82 +174,117 @@ namespace projekt.MainViewModel
 
         private void KodowanieRSA(string filePath, int? p, int? q)
         {
-            int n = (int)(p * q);
-            int phi = ((int)p - 1) * ((int)q - 1);
-            int e = 3;
-            while(NWD(e, phi) != 1)
+            if (q != null)
             {
-                e += 2;
-            }
-            int d = odwMod(e, phi);
-
-            if(File.Exists(filePath))
-            {
-                string[] lines = File.ReadAllLines(filePath);
-                string[] wynik = new string[lines.Length];
-
-                StreamWriter streamWriter;
-                streamWriter = File.AppendText(filePath.Substring(0, filePath.Length - filePath.Substring(filePath.LastIndexOf(@".")).Length)+"_zakodowany.txt");
-                for (int i=0; i<lines.Length; i++)
+                int n = (int)(p * q);
+                int phi = ((int)p - 1) * ((int)q - 1);
+                int e = 3;
+                while (NWD(e, phi) != 1)
                 {
-                    string[] tmp = lines[i].Split(' ');
-                    for(int j=0; j<tmp.Length; j++)
-                    {
-                        try
-                        {
-                            int liczba = Convert.ToInt32(tmp[j]);
-                            streamWriter.Write((Math.Pow(liczba, e) % n) + " ");
+                    e += 2;
+                }
+                int d = odwMod(e, phi);
 
-                        }
-                        catch(Exception)
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    string[] wynik = new string[lines.Length];
+
+                    StreamWriter streamWriter;
+                    streamWriter = File.AppendText(filePath.Substring(0, filePath.Length - filePath.Substring(filePath.LastIndexOf(@".")).Length) + "_zakodowany.txt");
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string[] tmp = lines[i].Split(' ');
+                        for (int j = 0; j < tmp.Length; j++)
                         {
-                            streamWriter.WriteLine();
-                            for (int k=0; k<tmp[j].Length; k++)
+
+                            for (int k = 0; k < tmp[j].Length; k++)
                             {
                                 int liczba = Convert.ToInt32(tmp[j][k]);
                                 streamWriter.Write((Math.Pow(liczba, e) % n) + " ");
                             }
-                            
+                            streamWriter.WriteLine();
                         }
+
                     }
-
+                    streamWriter.Close();
+                    MessageBox.Show("Klucz publiczny: (" + e + ";" + n + ")");
+                    MessageBox.Show("Klucz prywatny: (" + d + ";" + n + ")");
                 }
-                streamWriter.Close();
-                MessageBox.Show("Klucz publiczny: (" + e + ";" + n + ")");
-
-            }
-            else
-            {
-                MessageBox.Show("Taki plik nie istnieje!!!");
+                else
+                {
+                    MessageBox.Show("Taki plik nie istnieje!!!");
+                }
             }
         }
 
-        private void DekodowanieRSA(string pathFile, int? e, int? n)
+        private void KodowanieRSA1(string filePath, int e, int n)
         {
-            if(File.Exists(pathFile))
+            if (n != 0)
             {
-                StreamWriter streamWriter;
-                streamWriter = File.AppendText(pathFile);
-                string[] lines = File.ReadAllLines(pathFile);
-                for(int i=0; i<lines.Length; i++)
+                if (File.Exists(filePath))
                 {
-                    string[] tmp = lines[i].Split(' ');
-                    for(int j=0; j<tmp.Length; j++)
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    StreamWriter streamWriter;
+                    streamWriter = File.AppendText(filePath.Substring(0, filePath.Length - filePath.Substring(filePath.LastIndexOf(@".")).Length) + "_zakodowany.txt");
+                    for (int i = 0; i < lines.Length; i++)
                     {
-                        try
+                        string[] tmp = lines[i].Split(' ');
+                        for (int j = 0; j < tmp.Length; j++)
                         {
-                            int c = Convert.ToInt32(tmp[j]);
-                            string t = Convert.ToString((Math.Pow(c, (int)e) % n));
-                            streamWriter.Write(t);
+
+                            for (int k = 0; k < tmp[j].Length; k++)
+                            {
+                                int liczba = Convert.ToInt32(tmp[j][k]);
+                                streamWriter.Write((Math.Pow(liczba, e) % n) + " ");
+                            }
+                            streamWriter.WriteLine();
                         }
-                        catch (Exception) { }
+
                     }
-                    streamWriter.WriteLine();
+                    streamWriter.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Taki plik nie istnieje!!!");
                 }
             }
-            else
+        }
+
+        private void DekodowanieRSA(string pathFile, int d, int n)
+        {
+            if (n != 0)
             {
-                MessageBox.Show("Nie ma takiego pliku!!!");
+                if (File.Exists(pathFile))
+                {
+                    StreamWriter streamWriter;
+                    streamWriter = File.AppendText(pathFile.Substring(0, pathFile.Length - pathFile.Substring(pathFile.LastIndexOf(@"\") + 1).Length) + "dane_dekodowane.txt");
+                    string[] lines = File.ReadAllLines(pathFile);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string[] tmp = lines[i].Split(' ');
+                        for (int j = 0; j < tmp.Length; j++)
+                        {
+                            try
+                            {
+                                int c = Convert.ToInt32(tmp[j]);
+                                int wynik = 1;
+                                for (int k = 0; k < d; k++)
+                                    wynik = (wynik * (c % n)) % n;
+                                streamWriter.Write((char)wynik);
+                            }
+                            catch (Exception) { }
+
+                        }
+                        streamWriter.WriteLine();
+                    }
+                    streamWriter.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Nie ma takiego pliku!!!");
+                }
             }
         }
 
